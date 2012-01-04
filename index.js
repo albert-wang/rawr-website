efaccd638b41bef11e6f13db3c69200e27358e4b
Albert Wang <albertywang@gmail.com>
2012-01-03 20:02:49 -0600

    Fixing package.json

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var http      = require("http");
-var util      = require("util");
+var nodeutil  = require("util");
 var mu        = require("mu2");
-
+var express   = require("express");
+var render    = require("./utils/rendering.js")
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain' });
 
 	var greeting = mu.compileText("Hello {{name}}\n");
-	
-	mu.renderText("Hello, {{name}}\n", { name : "World" }, null)
-		.on('data', function(c) { res.write(c); })
-		.on('end', function() { res.end(); });
-
+    render.to(res, greeting, { name : "World" }, null, function()
+    {
+        res.end();
+    });
 }).listen(23296);
 
 
