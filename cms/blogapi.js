1b4f16fa9abeb02df7b7df21ed78cb14e985f48a
Albert Wang <albertywang@gmail.com>
2012-01-04 00:59:47 -0600

    Adding more implementation in the blog API, and then updating the schema

--- cms/blogapi.js
+++ cms/blogapi.js
@@ -4,6 +4,10 @@
     var setup = require("../utils/setup.js");
     var fe    = require("flow").exec;
 
+    var DEFAULT_ID = 1;
+    var INVALID_ID = 0;
+
+    //Two functions to get values by ID.
     function getCategoryIDByName(name, cb)
     {
         fe(function()
@@ -11,12 +15,12 @@
             setup.getConnection(this);
         }, function(err, client)
         {
-            if (err) { cb(err); return undefined; }
+            if (err) { cb(err, DEFAULT_ID); return undefined; }
 
             var category = name || "uncategorized";
 
             client.query({
-                name : "select tag by name", 
+                name : "select category by name", 
                 text : "SELECT id FROM blog_categories WHERE name = $1", 
                 values : [category]
             }, this);
@@ -24,9 +28,41 @@
         {
             if (err || result.rows.length == 0)
             {
-                cb(err, 1);
+                cb(err, DEFAULT_ID);
                 return undefined;
             }
+            cb(undefined, result.rows[0].id);
+        });
+    }
+
+    function getTagIDByName(name, cb)
+    {
+        fe(function()
+        {
+            setup.getConnection(this);
+        }, function(err, client)
+        {
+            if (err) { cb(err, INVALID_ID); return undefined; }
+            if (!name) { cb("No name provided", INVALID_ID); return undefined; }
+
+            client.query({
+                name : "select tag by name", 
+                text : "SELECT id FROM blog_tags WHERE name = $1", 
+                values : [name]
+            }, this);
+        }, function(err, result)
+        {
+            if (err)
+            {
+                return cb(err, INVALID_ID);
+                return undefined;
+            }
+
+            if (result.rows.length == 0)
+            {
+                return cb("No tag with the name: " + name + " was found.", INVALID_ID);
+            }
+
             cb(undefined, result.rows[0].id);
         });
     }
@@ -54,18 +90,65 @@
 
             client.query({
                 name : "insert post", 
-                text : "INSERT INTO blog_posts (title, category, content, time) VALUES ($1, $2, $3, NOW())", 
+                text : 
+                    "INSERT INTO blog_posts (title, category, content, time) VALUES ($1, $2, $3, NOW()) " + 
+                    " RETURNING id ",
                 values: [title, catID, content]
             }, this);
          }, function (err, result)
          {
-             cb(err);
+             if (err)
+             {
+                 cb(err, INVALID_ID);
+                 return undefined;
+             }
+
+             if (result.length == 0)
+             {
+                 //Weird.
+                 cb("The insert query returned no ids.", INVALID_ID);
+                 return undefined;
+             }
+
+             cb(undefined, result.rows[0].id);
          });
     }
 
+    function tag(params, cb)
+    {
+        fe(function()
+        {
+            setup.getConnection(this)
+        }, function(err, client)
+        {
+            if (err) { cb(err); return undefined; }
+        });
+    }
+
+    function associateTagsWithPost(postID, tags, cb)
+    {
+        fe(function()
+        {
+            setup.getConnection(this);
+        }, function(err, client)
+        {
+            if (err)
+            {
+                cb(err);
+                return undefined;
+            }
+
+            client.query({
+                name : "associate tags with post", 
+
+            }, this);
+        });
+    }
+
     module.exports = {
-        post : post,
-        getCategoryIDByName : getCategoryIDByName
+        post                    : post,
+        getCategoryIDByName     : getCategoryIDByName,
+        getTagIDByName          : getTagIDByName
     };
 })();
 
--- schema.sql
+++ schema.sql
@@ -1,8 +1,16 @@
+
+-- Create the categories table and index
 CREATE TABLE IF NOT EXISTS blog_categories
     ( id        SERIAL NOT NULL UNIQUE PRIMARY KEY
     , name      VARCHAR(128)
     );
 
+CREATE INDEX blog_categories_name_index ON blog_categories
+    ( name
+    ); 
+
+-- Then insert the valid values into it.
+-- Make sure that there are no other values in it.
 DELETE FROM blog_categories;
 INSERT INTO blog_categories(name) VALUES
     ('uncategorized'),
@@ -10,6 +18,7 @@ INSERT INTO blog_categories(name) VALUES
     ('araboth')
     ;
 
+--The post table
 CREATE TABLE IF NOT EXISTS blog_posts 
     ( id        SERIAL NOT NULL UNIQUE PRIMARY KEY
     , title     VARCHAR(128)
@@ -18,6 +27,8 @@ CREATE TABLE IF NOT EXISTS blog_posts
     , time      TIMESTAMP
     );
 
+--The comment table.
+--Note that comments can nest within themselves.
 CREATE TABLE IF NOT EXISTS blog_comments
     ( id             SERIAL NOT NULL UNIQUE PRIMARY KEY
     , parent_post    INTEGER REFERENCES blog_posts(id)
@@ -27,9 +38,14 @@ CREATE TABLE IF NOT EXISTS blog_comments
     , time           TIMESTAMP
     );
 
+--Create the tag table, index, and bridge
 CREATE TABLE IF NOT EXISTS blog_tags
     ( id        SERIAL NOT NULL UNIQUE PRIMARY KEY
     , name      VARCHAR(128)
+    );
+
+CREATE INDEX blog_tags_name_index ON blog_tags 
+    ( name
     );
 
 CREATE TABLE IF NOT EXISTS blog_tag_bridge
