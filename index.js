1e572f05c462e799da83b9e6c1b00ba1d84cfee4
Albert Wang <albertywang@gmail.com>
2012-01-05 10:41:52 -0600

    Layouts

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ var app = setup.setup(function(app)
     //Routing
     app.get("/", function(req, res)
     {
-        fe(function()
-        {
-            setup.getConnection(this);
-        }, function(err, client)
-        {
-            this();
-        });
-
         res.render("container.html", { Title : "Title" });
     });
 });
