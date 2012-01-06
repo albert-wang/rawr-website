416a4b2789b97bca964524b6be86d7ca2dbb9636
Rraawwrr <rraawwrr@web311.webfaction.com>
2012-01-06 06:48:06 +0100

    Adding memcached launching

--- launch.sh
+++ launch.sh
@@ -3,7 +3,13 @@
 mkdir -p logs
 
 if [ "$(pidof node)" ]; then
-	echo "Already exists..."
+	echo "Node already running..."
 else
 	/home/rraawwrr/bin/node /home/rraawwrr/webapps/node/index.js & 
+fi
+
+if [ "$(pidof memcached)" ]; then
+	echo "Memcached already running..."
+else 
+	memcached -m -d 10 -s /home/rraawwrr/memcached.sock &
 fi
