e2388ac54d3d66b7530524d664996ad135bdb67e
Albert Wang <albertywang@gmail.com>
2012-01-08 01:10:10 -0600

    Common.js

---
+++ common.js
@@ -0,0 +1,51 @@
+
+(function()
+{
+	function navigation(active, cb)
+	{
+		 
+        var navigation = [
+            {
+                link: '/blog/all', 
+                title: 'Blog', 
+                text: 'Read the blog', 
+            },
+            {
+                link: "/gallery",
+                title: "Gallery", 
+                text: "Browse the gallery", 
+            },
+            {
+                link: "/projects",
+                title: "Projects", 
+                text: "What I'm working on", 
+
+            },
+            {
+                link: '/downloads',
+                title: 'Downloads',
+                text: 'most recent builds and tools',
+            }, 
+            {
+                link: "/about", 
+                title: "About", 
+                text: "About Rawr Productions"
+            }
+        ];
+
+        for (var i = 0; i < navigation.length; ++i)
+        {
+            if (navigation[i].title === active) 
+            {
+                navigation[i].active = true;
+            }
+        }
+
+        cb(undefined, navigation);
+        return undefined;
+	}
+
+	module.exports = {
+		navigation : navigation
+	}
+})();
