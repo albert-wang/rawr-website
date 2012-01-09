5d7fef00b76e91aff700dba42aacdbb78766ddea
Albert Wang <albertywang@gmail.com>
2012-01-08 18:35:22 -0600

    Missing files

---
+++ gallery.js
@@ -0,0 +1,31 @@
+
+(function()
+{
+	var gapi 	= require("./cms/galleryapi.js");
+	var common 	= require("./common.js");
+
+
+	function home(req, res, setup)
+	{
+		common.navigation("Gallery", function(err, nav)
+		{
+			gapi.getAllGalleriesWithOneImage(function(err, galleries)
+			{
+				res.render("gallery_home.html", {
+					navigation_blocks: nav,
+					galleries: galleries,
+					feature: {
+						title 	: "Araboth Gallery",
+						content : "The most recent screenshots from my project, Araboth",
+						link    : "/gallery/araboth"
+					}
+				});
+			});
+		});
+	}
+
+
+	module.exports = {
+		home : home
+	}
+})();
